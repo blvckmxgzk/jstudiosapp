19db76d8132f5f675a05bb174d5ecb00a65988f5
Blackmagik <codejast09@hotmail.com>
2025-12-03 07:09:39 +0000

    commit

---
+++ jsstudiosapp/android/app/src/main/kotlin/com/example/jsstudiosapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jsstudiosapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- jsstudiosapp/test/widget_test.dart
+++ jsstudiosapp/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:jstudiosapp/main.dart';
+import 'package:jsstudiosapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
